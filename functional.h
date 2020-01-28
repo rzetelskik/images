268b38110be4fd84b72c99fc3d1628ef89e4e760
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2020-01-28 15:19:59 +0100

    lift and compose

--- functional.h
+++ functional.h
@@ -1,4 +1,29 @@
 #ifndef FUNCTIONAL_H
 #define FUNCTIONAL_H
 
+template<typename F, typename ... Fs>
+inline auto compose(F f, Fs ... fs) {
+    return compose(f, compose(fs...));
+}
+
+template<typename F, typename G>
+inline auto compose(F f, G g) {
+    return [=](auto x) { return g(f(x)); };
+}
+
+inline auto compose() {
+    return [](auto x) { return x; };
+}
+
+template<typename H, typename ... Fs>
+inline auto lift(H h, Fs ... fs) {
+    return [=](auto p) { return h(fs(p) ...); };
+}
+
+template<typename H>
+inline auto lift(H h) {
+    return [=](auto p) { return h(p); };
+}
+
+
 #endif //FUNCTIONAL_H
