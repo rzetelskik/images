38b07b2e48d5e6fe0affc8973db0b5e1f451db2e
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2020-02-16 17:09:19 +0100

    final

--- functional.h
+++ functional.h
@@ -1,29 +1,22 @@
 #ifndef FUNCTIONAL_H
 #define FUNCTIONAL_H
 
+template<typename ... Fs>
+inline auto compose(const Fs &... fs);
+
 template<typename F, typename ... Fs>
-inline auto compose(F f, Fs ... fs) {
-    return compose(f, compose(fs...));
+inline auto compose(const F &f, const Fs &... fs) {
+    return [=](auto x) { return compose(fs ...)(f(x)); };
 }
 
-template<typename F, typename G>
-inline auto compose(F f, G g) {
-    return [=](auto x) { return g(f(x)); };
-}
-
+template<>
 inline auto compose() {
     return [](auto x) { return x; };
 }
 
 template<typename H, typename ... Fs>
-inline auto lift(H h, Fs ... fs) {
+inline auto lift(const H &h, const Fs &... fs) {
     return [=](auto p) { return h(fs(p) ...); };
 }
-
-template<typename H>
-inline auto lift(H h) {
-    return [=](auto p) { return h(p); };
-}
-
 
 #endif //FUNCTIONAL_H
--- images.h
+++ images.h
@@ -20,86 +20,82 @@ using Blend = Base_image<Fraction>;
 
 template<typename T>
 inline Base_image<T> constant(const T t) {
-    return [=]([[gnu::unused]] Point p) { return t; };
+    return [=]([[gnu::unused]] const Point p) { return t; };
 }
 
 template<typename T>
-Base_image<T> rotate(Base_image<T> image, double phi) {
-    auto transform = [=](Point p) {
+Base_image<T> rotate(const Base_image<T> &image, const double &phi) {
+    auto transform = [=](const Point p) {
         return Point(p.first * cos(phi) + p.second * sin(phi), p.second * cos(phi) - p.first * sin(phi));
     };
 
-    return [=](Point p) { return lift(image, transform)(p); };
+    return [=](const Point p) { return lift(image, transform)(p); };
 }
 
 template<typename T>
-Base_image<T> translate(Base_image<T> image, Vector v) {
-    auto transform = [=](Point p) { return Point(p.first - v.first, p.second - v.second); };
+Base_image<T> translate(const Base_image<T> &image, const Vector &v) {
+    auto transform = [=](const Point p) { return Point(p.first - v.first, p.second - v.second); };
 
-    return [=](Point p) { return lift(image, transform)(p); };
+    return [=](const Point p) { return lift(image, transform)(p); };
 }
 
 template<typename T>
-Base_image<T> scale(Base_image<T> image, double s) {
-    auto transform = [=](Point p) { return Point(p.first / s, p.second / s); };
+Base_image<T> scale(const Base_image<T> &image, const double &s) {
+    auto transform = [=](const Point p) { return Point(p.first / s, p.second / s); };
 
-    return [=](Point p) { return lift(image, transform)(p); };
+    return [=](const Point p) { return lift(image, transform)(p); };
 }
 
 template<typename T>
-Base_image<T> circle(Point q, double r, T inner, T outer) {
-    return [=](Point p) { return distance(p, q) <= r ? inner : outer; };
+Base_image<T> circle(const Point &q, const double &r, const T &inner, const T &outer) {
+    return [=](const Point p) { return distance(p, q) <= r ? inner : outer; };
 }
 
 template<typename T>
-Base_image<T> checker(double d, T this_way, T that_way) {
-    return [=](Point p) {
+Base_image<T> checker(const double &d, const T &this_way, const T &that_way) {
+    return [=](const Point p) {
         return (static_cast<int>(std::floor(p.first / d) + std::floor(p.second / d)) & 1) ? that_way : this_way;
     };
 }
 
 template<typename T>
-Base_image<T> polar_checker(double d, int n, T this_way, T that_way) {
-    return [=](Point p) {
-        auto sc = [=](Point p) { return Point(p.first, (p.second * n * d) / (2 * M_PI)); };
+Base_image<T> polar_checker(const double &d, const int &n, const T &this_way, const T &that_way) {
+    return [=](const Point p) {
+        auto sc = [=](const Point p) { return Point(p.first, (p.second * n * d) / (2 * M_PI)); };
         return compose(to_polar, sc, checker(d, this_way, that_way))(p);
     };
 }
 
 template<typename T>
-Base_image<T> rings(Point q, double d, T this_way, T that_way) {
-    return [=](Point p) {
+Base_image<T> rings(const Point &q, const double &d, const T &this_way, const T &that_way) {
+    return [=](const Point p) {
         return (static_cast<int>(std::floor(distance(p, q) / d)) & 1) ? that_way : this_way;
     };
 }
 
 template<typename T>
-Base_image<T> vertical_stripe(double d, T this_way, T that_way) {
-    return [=](Point p) { return (std::abs(p.first) <= d / 2) ? this_way : that_way; };
+Base_image<T> vertical_stripe(const double &d, const T &this_way, const T &that_way) {
+    return [=](const Point p) { return (std::abs(p.first) <= d / 2) ? this_way : that_way; };
 }
 
-inline Image cond(Region region, Image this_way, Image that_way) {
+inline Image cond(const Region &region, const Image &this_way, const Image &that_way) {
     auto h = [=](auto a, auto b, auto c) { return a ? b : c; };
 
-    return [=](Point p) { return lift(h, region, this_way, that_way)(p); };
+    return [=](const Point p) { return lift(h, region, this_way, that_way)(p); };
 }
 
-inline Image lerp(Blend blend, Image this_way, Image that_way) {
+inline Image lerp(const Blend &blend, const Image &this_way, const Image &that_way) {
     auto h = [=](auto a, auto b, auto c) { return a.weighted_mean(b, c); };
 
-    return [=](Point p) { return lift(h, this_way, that_way, blend)(p); };
+    return [=](const Point p) { return lift(h, this_way, that_way, blend)(p); };
 }
 
-inline Image darken(Image image, Blend blend) {
-    auto h = [=](auto a, auto b) { return Colors::black.weighted_mean(a, b); };
-
-    return [=](Point p) { return lift(h, image, blend)(p); };
+inline Image darken(const Image &image, const Blend &blend) {
+    return lerp(blend, image, constant(Colors::black));
 }
 
-inline Image lighten(Image image, Blend blend) {
-    auto h = [=](auto a, auto b) { return Colors::white.weighted_mean(a, b); };
-
-    return [=](Point p) { return lift(h, image, blend)(p); };
+inline Image lighten(const Image &image, const Blend &blend) {
+    return lerp(blend, image, constant(Colors::white));
 }
 
 
